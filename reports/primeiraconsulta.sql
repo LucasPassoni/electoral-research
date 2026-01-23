9c0f250aa8fe8ebf9dbd2e130a040f3e0bf0f828
Lucas Passoni <lcspssn@gmail.com>
2026-01-23 19:00:25 +0000

    primeira consulta bigquery

---
+++ reports/primeiraconsulta.sql
@@ -0,0 +1,3 @@
+SELECT ano_emenda, tipo_emenda, nome_autor_emenda, localidade, nome_funcao,nome_programa, valor_empenhado, valor_liquidado, valor_pago  
+FROM `basedosdados.br_cgu_emendas_parlamentares.microdados` 
+LIMIT 1000; 

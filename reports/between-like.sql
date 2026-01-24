8a0b6023d04602bebb443d7d4495c5fd606f8848
Lucas Passoni <lcspssn@gmail.com>
2026-01-24 14:09:55 +0000

    uso between, like

---
+++ reports/between-like.sql
@@ -0,0 +1,5 @@
+SELECT valor_empenhado, tipo_emenda, sigla_uf_gasto, valor_pago
+FROM `basedosdados.br_cgu_emendas_parlamentares.microdados`
+WHERE (valor_empenhado BETWEEN 500000 AND 5000000) AND (tipo_emenda LIKE "Emenda%" AND sigla_uf_gasto LIKE "P%")
+ORDER BY valor_empenhado DESC
+LIMIT 1000;
